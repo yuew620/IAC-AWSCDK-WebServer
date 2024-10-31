e498bb9c85ff5ada19d94488302ae1d780e20169
Wang Yue <yyuewang@amazon.com>
2024-10-31 14:14:37 +0800

    empty cdk project

---
+++ test/iac-awscdk-web_server.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IacAwscdkWebServer from '../lib/iac-awscdk-web_server-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/iac-awscdk-web_server-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IacAwscdkWebServer.IacAwscdkWebServerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
